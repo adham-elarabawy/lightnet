4457d56a2461c1ec6df03299c13527438bcfb00d
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-07-23 15:44:47 -0700

    added fps counter

--- darknet_video.py
+++ darknet_video.py
@@ -128,7 +128,8 @@ def YOLO(args):
     while True:
         prev_time = time.time()
         ret, frame_read = cap.read()
-        print(metaMain.classes)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
+            break
         if(ret):
             frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
             frame_resized = cv2.resize(frame_rgb,
@@ -141,16 +142,13 @@ def YOLO(args):
 
             detections = darknet.detect_image(
                 netMain, metaMain, darknet_image, thresh=args.confidence, nms=args.nms_thresh, debug=False)
-            print(detections)
             image = cvDrawBoxes(detections, frame_resized)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             # out.write(image)
-            print(1/(time.time()-prev_time))
+            print("fps: " + str(1/(time.time()-prev_time)))
             if(args.show):
                 cv2.imshow('Demo', image)
                 cv2.waitKey(3)
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
-            break
     cap.release()
     out.release()
 
