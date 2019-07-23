fa4fb11d05f868352efdbc4fbb88da252a729861
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-07-23 13:09:41 -0700

    added argument control

--- darknet_video.py
+++ darknet_video.py
@@ -6,6 +6,37 @@ import cv2
 import numpy as np
 import time
 import darknet
+import argparse
+
+
+def arg_parse():
+    """
+    Parse arguements to the detect module
+
+    """
+
+    parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
+
+    parser.add_argument("--out", dest='output', help="video path to store the video in(include the desired video name and type[avi])",
+                        default="output.avi", type=str),
+    parser.add_argument("--src", dest='source', help="path of the video that the model is being tested on(include the desired video name and type)",
+                        default="input.avi", type=str),
+    parser.add_argument("--bs", dest="bs", help="Batch size", default=1)
+    parser.add_argument("--confidence", dest="confidence",
+                        help="Object Confidence to filter predictions", default=0.25)
+    parser.add_argument("--nms_thresh", dest="nms_thresh",
+                        help="NMS Threshhold", default=0.45),
+    parser.add_argument("--cfg", dest='cfg', help="Config file",
+                        default="cfg/yolov3.cfg", type=str),
+    parser.add_argument("--weights", dest='weightsfile', help="weightsfile",
+                        default="yolov3.weights", type=str),
+    parser.add_argument("--meta", dest='data', help="path to the .data file detailing the model metadata",
+                        default="cfg/model.data", type=str)
+    parser.add_argument('--show', dest='show', action='store_true')
+    parser.add_argument('--dont_show', dest='show', action='store_false')
+    parser.set_defaults(feature=False)
+    return parser.parse_args()
+
 
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
@@ -39,12 +70,15 @@ metaMain = None
 altNames = None
 
 
-def YOLO():
+def YOLO(args):
 
     global metaMain, netMain, altNames
-    configPath = "./cfg/yolov3.cfg"
-    weightPath = "./yolov3.weights"
-    metaPath = "./cfg/coco.data"
+    configPath = args.cfg
+    weightPath = args.weights
+    metaPath = args.data
+
+    print("**Using:\n\tCFG: " + configPath + "\n\tWeights: " +
+          weightPath + "\n\tMetadata: " + metaPath)
     if not os.path.exists(configPath):
         raise ValueError("Invalid config path `" +
                          os.path.abspath(configPath)+"`")
@@ -56,7 +90,7 @@ def YOLO():
                          os.path.abspath(metaPath)+"`")
     if netMain is None:
         netMain = darknet.load_net_custom(configPath.encode(
-            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+            "ascii"), weightPath.encode("ascii"), 0, args.bs)  # batch size = 1
     if metaMain is None:
         metaMain = darknet.load_meta(metaPath.encode("ascii"))
     if altNames is None:
@@ -80,17 +114,17 @@ def YOLO():
         except Exception:
             pass
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("test.mp4")
-    cap.set(3, 1280)
-    cap.set(4, 720)
+    cap = cv2.VideoCapture(args.source)
+    cap.set(3, 1920)
+    cap.set(4, 1080)
     out = cv2.VideoWriter(
-        "output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
+        args.output, cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
         (darknet.network_width(netMain), darknet.network_height(netMain)))
     print("Starting the YOLO loop...")
 
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
-                                    darknet.network_height(netMain),3)
+                                       darknet.network_height(netMain), 3)
     while True:
         prev_time = time.time()
         ret, frame_read = cap.read()
@@ -100,16 +134,20 @@ def YOLO():
                                     darknet.network_height(netMain)),
                                    interpolation=cv2.INTER_LINEAR)
 
-        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+        darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
 
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        detections = darknet.detect_image(
+            netMain, metaMain, darknet_image, thresh=args.confidence, nms=args.nms_thresh)
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         print(1/(time.time()-prev_time))
-        cv2.imshow('Demo', image)
+        if(args.show):
+            cv2.imshow('Demo', image)
         cv2.waitKey(3)
     cap.release()
     out.release()
 
+
 if __name__ == "__main__":
-    YOLO()
+    args = arg_parse()
+    YOLO(args)
