6521530371dbcaec9aa40fe2cf4a021d37515a37
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-07-23 13:42:03 -0700

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -50,7 +50,7 @@ class METADATA(Structure):
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
 lib = CDLL(
-    "/mnt/nas01/workspace_share/cnn/yolo_alexey/darknet/libdarknet.so", RTLD_GLOBAL)
+    "libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
