b7d3ff74adb0c2aa2d1f3d3b9657690cccbdb471
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-07-23 15:40:07 -0700

    debug 7

--- darknet_video.py
+++ darknet_video.py
@@ -136,11 +136,11 @@ def YOLO(args):
                                         darknet.network_height(netMain)),
                                        interpolation=cv2.INTER_LINEAR)
 
-            darknet.copy_image_from_bytes(frame_resized.tobytes(),
-                                          darknet_image)
+            darknet.copy_image_from_bytes(
+                darknet_image, frame_resized.tobytes())
 
             detections = darknet.detect_image(
-                netMain, metaMain, darknet_image, thresh=args.confidence, nms=args.nms_thresh)
+                netMain, metaMain, darknet_image, thresh=args.confidence, nms=args.nms_thresh, debug=True)
             print(detections)
             image = cvDrawBoxes(detections, frame_resized)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
