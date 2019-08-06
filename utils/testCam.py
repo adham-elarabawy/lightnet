26d81e4acebdd31f9c52b41537b61bc4e9d6e4eb
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-08-06 12:29:49 -0700

    Update testCam.py

--- utils/testCam.py
+++ utils/testCam.py
@@ -7,11 +7,12 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if(ret):
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
