adc20bf0bb1adbb79d5b4658d05636c2a0cfaf5a
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-08-05 18:01:10 -0700

    Update darknet_detect.py

--- darknet_detect.py
+++ darknet_detect.py
@@ -272,7 +272,7 @@ def YOLO(args):
     if fileType == 0:  # input is a video
         if DEBUG_PRINT:
             print('Validated: Source input is a video.')
-        if args.source == "0":
+        if args.source == '0':
             args.source = 0
         cap = cv2.VideoCapture(args.source)  # set to 0 to use webcam input
         cap.set(3, 1920)
